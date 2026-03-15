633a92c99a35ce59b40b37ea2af1c98877244596
maskedsyntax <aftaab@aftaab.xyz>
2026-03-15 15:58:49 +0530

    Enable OpenMP support on macOS via Homebrew libomp

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
 import sys
+import subprocess
+import os
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import pybind11
+
+def get_brew_prefix():
+    try:
+        return subprocess.check_output(["brew", "--prefix"], encoding="utf8").strip()
+    except Exception:
+        return "/usr/local"
 
 class PybindBuildExt(build_ext):
     """Custom build_ext to ensure specific compiler flags."""
     def build_extensions(self):
         ct = self.compiler.compiler_type
         opts = []
+        link_opts = []
+        
         if ct == 'unix':
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append('-std=c++17')
             opts.append('-fvisibility=hidden')
-            if sys.platform != 'darwin':
+            
+            if sys.platform == 'darwin':
+                # Try to enable OpenMP on macOS using libomp from Homebrew
+                brew_prefix = get_brew_prefix()
+                omp_prefix = os.path.join(brew_prefix, "opt", "libomp")
+                if os.path.exists(omp_prefix):
+                    opts.append('-Xpreprocessor')
+                    opts.append('-fopenmp')
+                    opts.append(f'-I{omp_prefix}/include')
+                    link_opts.append(f'-L{omp_prefix}/lib')
+                    link_opts.append('-lomp')
+            else:
                 opts.append('-fopenmp')
+                link_opts.append('-fopenmp')
+                
         elif ct == 'msvc':
             opts.append('/DVERSION_INFO=\"%s\"' % self.distribution.get_version())
             opts.append('/std:c++17')
@@ -21,8 +44,7 @@ class PybindBuildExt(build_ext):
         
         for ext in self.extensions:
             ext.extra_compile_args = opts
-            if ct == 'unix' and sys.platform != 'darwin':
-                ext.extra_link_args = ['-fopenmp']
+            ext.extra_link_args = link_opts
         
         build_ext.build_extensions(self)
 
